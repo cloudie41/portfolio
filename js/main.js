9df407c31ea42bb8410b230789d65ed8b1c4c8a2
cloudie41 <32173689+cloudie41@users.noreply.github.com>
2017-09-22 16:22:04 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-// Make it rain!!
+$('.hamburger').on('click', function(){
+  console.log('button is clicked')
+  $('.nav-links').addClass('active');
+});
+
+$('.close').on('click', function(){
+  console.log('button is clicked')
+  $('.nav-links').removeClass("active");
+});
